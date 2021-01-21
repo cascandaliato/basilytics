eb32381f18a37094307df35ebbc8f96aabf1ca62
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2021-01-21 19:59:45 +0100

    move geo location db under public

--- src/utils/get-country.ts
+++ src/utils/get-country.ts
@@ -2,7 +2,7 @@ import maxmind, { CountryResponse } from 'maxmind'
 import path from 'path'
 
 export const getCountry = async (ip: string): Promise<string> => {
-  const geoLiteDB = path.join(path.resolve('./utils'), 'GeoLite2-Country.mmdb')
+  const geoLiteDB = path.join(path.resolve('./public'), 'GeoLite2-Country.mmdb')
   const lookup = await maxmind.open<CountryResponse>(geoLiteDB)
   return lookup.get(ip)?.country.names.en
 }
